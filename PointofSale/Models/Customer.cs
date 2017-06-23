cf0f4a9b154fc24f15d7ecee6b5d4f6883cdc5b6
Vince Mendiola <vinzmendiola@gmail.com>
2017-06-23 14:18:55 +0800

    Customer Model

---
+++ PointofSale/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointofSale.Models
+{
+    public class Customer : Person
+    {
+
+        public new bool save()
+        {
+            try
+            {
+                
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ PointofSale/Models/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PointofSale.Interfaces;
+
+namespace PointofSale.Models
+{
+    public class Person : ICrud
+    {
+        public int id { get; set; }
+        public string code { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime dateofBirth { get; set; }
+        public string email { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string contactNo { get; set; }
+
+        public bool deleteOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool deleteAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool updateOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
