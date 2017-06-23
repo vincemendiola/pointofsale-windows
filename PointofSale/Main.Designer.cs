75a58433fd6725fe7e8840d08a2908137847553b
Vince Mendiola <vinzmendiola@gmail.com>
2017-06-23 09:00:32 +0800

    Inventory Model and ICrud Interface

--- PointofSale/Main.Designer.cs
+++ PointofSale/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PointofSale
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
---
+++ PointofSale/Models/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PointofSale.Interfaces;
+
+namespace PointofSale.Models
+{
+    class Inventory : ICrud
+    {
+        public int id { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public int itemType { get; set; }
+
+        public bool deleteOne(int id)
+        {
+            return true;
+        }
+
+        public bool deleteAll()
+        {
+            return true;
+        }
+
+        public bool updateOne()
+        {
+            return true;
+        }
+
+        public bool save()
+        {
+            return true;
+        }
+    }
+}
